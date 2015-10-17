95143041f7c5e62660c82fc04a210d93ccb01d5f
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-17 21:59:51 +0200

    Make request respond with json

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,10 +5,11 @@ module Lib
     ) where
 
 import Web.Scotty
+import Data.Text
 
 api :: IO ()
 api = scotty 3000 $ do
     get "/" homeAction
 
 homeAction :: ActionM ()
-homeAction = html "Hello World"
+homeAction = json ("Hello World" :: Text)
