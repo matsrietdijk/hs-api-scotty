fdcac5a2548eb7ab7be404f153ce59c6eb82d037
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-17 22:47:49 +0200

    Add basic post model data with toJSON

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,11 +5,31 @@ module Lib
     ) where
 
 import Web.Scotty
-import Data.Text
+import Data.Aeson hiding (json)
+import Data.Text hiding (length)
 
 api :: IO ()
 api = scotty 3000 $ do
     get "/" homeAction
 
+data Post = Post
+            { postId :: Maybe Integer,
+              postTitle :: Text,
+              postBody :: Text
+            }
+            deriving (Show)
+
+instance ToJSON Post where
+    toJSON (Post mpId pTitle pBody) =
+        object [ "id" .= mpId,
+                 "title" .= pTitle,
+                 "body" .= pBody
+               ]
+
 homeAction :: ActionM ()
-homeAction = json ("Hello World" :: Text)
+homeAction = json $ object [ "posts" .= posts,
+                             "meta" .= meta
+                           ]
+    where posts = [Post (Just 1) "title 1" "body 1"]
+          meta = object [ "count" .= length posts
+                        ]
