4127c714a50c03c9553063955f4daef53b5cb6d2
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-18 23:39:46 +0200

    Add postgresql simple for database connection

--- src/Api.hs
+++ src/Api.hs
@@ -1,18 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Api
     ( api
     ) where
 
-import Web.Scotty (ActionM, scotty, get, json, param)
+import Web.Scotty.Trans (ActionT, scottyT, get, json, param, status)
 import Data.Aeson (ToJSON (..), object, (.=))
-import Data.Text (Text)
+import Data.Text.Lazy (Text)
+import Database.PostgreSQL.Simple (Connection, ConnectInfo (..), Only (..), connect, defaultConnectInfo, query)
+import Control.Monad.Reader (ReaderT, MonadReader, runReaderT, asks)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Class (lift)
+import Network.HTTP.Types.Status (notFound404)
+
+data Config = Config
+              { dbConn :: Connection
+              }
+
+newtype ConfigM a = ConfigM
+                 { runConfigM :: ReaderT Config IO a
+                 } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Config)
+
+getConfig = do
+    conn <- connect defaultConnectInfo { connectDatabase = "hs-api-scotty",
+                                         connectUser = "mats"
+                                       }
+    return $ Config conn
 
 api :: IO ()
-api = scotty 3000 $ do
-    get "/" homeAction
-    get "/:id" showAction
+api = do
+    conf <- getConfig
+    let r m = runReaderT (runConfigM m) conf
+    scottyT 3000 r $ do
+        get "/" homeAction
+        get "/:id" showAction
 
 data Post = Post
             { postId :: Maybe Integer,
@@ -28,16 +52,38 @@ instance ToJSON Post where
                  "body" .= pBody
                ]
 
-homeAction :: ActionM ()
-homeAction = json $ object [ "posts" .= posts,
-                             "meta" .= meta
-                           ]
-    where posts = [Post (Just 1) "title 1" "body 1"]
-          meta = object [ "count" .= length posts
-                        ]
-showAction :: ActionM ()
-showAction = do
-    _id :: Integer <- param "id"
-    json $ object [ "post" .= single_post
+type Action = ActionT Text ConfigM ()
+
+homeAction :: Action
+homeAction = do
+    ps <- allPosts
+    json $ object [ "posts" .= ps,
+                    "meta" .= meta ps
                   ]
-    where single_post = Post (Just 1) "title 1" "body 1"
+    where meta ps = object [ "count" .= length ps
+                           ]
+
+showAction :: Action
+showAction = do
+    id :: Integer <- param "id"
+    mp <- findPost id
+    case mp of
+        Just p -> json $ object [ "post" .= p
+                                ]
+        Nothing -> do
+            status notFound404
+            json ()
+
+runDB q p = do
+    conn <- lift $ asks dbConn
+    liftIO $ query conn q p
+
+allPosts = do
+    rows <- runDB "select * from posts" ()
+    return $ map (\(id, title, body) -> Post (Just id) title body) rows
+
+findPost id = do
+    rows <- runDB "select * from posts where id = ?" (Only id)
+    return $ firstPost rows
+    where firstPost ((id, title, body) : _) = Just $ Post (Just id) title body
+          firstPost _ = Nothing
